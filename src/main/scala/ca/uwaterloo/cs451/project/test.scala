946984ec8eab2cd2eeb361e4439998ab05a9ab37
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-11-11 13:42:14 -0500

    Update test.scala

--- src/main/scala/ca/uwaterloo/cs451/project/test.scala
+++ src/main/scala/ca/uwaterloo/cs451/project/test.scala
@@ -10,9 +10,9 @@ import org.apache.spark.SparkConf
 import org.rogach.scallop._
 
 class ConfStripesPMI(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
-  mainOptions = Seq(input, output, reducers)
-  val input = opt[String](descr = "input path", required = true)
-  val output = opt[String](descr = "output path", required = true)
+  mainOptions = Seq(input, output)
+  val input = opt[String](descr = "input path", required = false)
+  val output = opt[String](descr = "output path", required = false)
 //   val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
 //   val numExecutors = opt[Int](descr = "number of executors", required = false, default = Some(1))
 //   val executorCores = opt[Int](descr = "number of cores", required = false, default = Some(1))
@@ -26,18 +26,17 @@ object StripesPMI extends Tokenizer {
   def main(argv: Array[String]) {
     val args = new ConfStripesPMI(argv)
 
-    log.info("Input: " + args.input())
-    log.info("Output: " + args.output())
+//     log.info("Input: " + args.input())
+//     log.info("Output: " + args.output())
 //     log.info("Number of reducers: " + args.reducers())
 
     val conf = new SparkConf().setAppName("Compute Bigram Relative Frequency Stripes")
     val sc = new SparkContext(conf)
-   val threshold = args.threshold()
 
-    val outputDir = new Path(args.output())
-    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
+//     val outputDir = new Path(args.output())
+//     FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true)
     
-    val textFile = sc.textFile(args.input(), args.reducers())
+    val textFile = sc.textFile("data2/index.csv")
     .take(5)
     .foreach(line => {
       println(line)

db7445938bb1af32545d9a337d5e26ba0c9df879
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-11-25 16:56:25 -0500

    Update test.scala

--- src/main/scala/ca/uwaterloo/cs451/project/test.scala
+++ src/main/scala/ca/uwaterloo/cs451/project/test.scala
@@ -41,74 +41,74 @@ object test extends Tokenizer {
 //     WRS_PATH,WRS_ROW,CLOUD_COVER,NORTH_LAT,SOUTH_LAT,
 //     WEST_LON,EAST_LON,TOTAL_SIZE,BASE_URL
     val textFile = sc.textFile("customer_data.csv")
-    .flatMap(line => {
-      val tokens = line.split(',')
-      (tokens(0), tokens(1), tokens(2), tokens(3), tokens(4), tokens(5), tokens(6), tokens(7), tokens(8), tokens(9), tokens(10), tokens(11), tokens(12), tokens(13), tokens(14), tokens(15), tokens(16),tokens(17))
-    })
     
     textFile
+    .map(line => {
+      tokens = line.split(',')
+      tokens
+    })
     .map(line => {
       (line._2, 1)
     })
     .reduceByKey(_+_)
     .saveAsTextFile("numberOfProducts.txt")
     
-    textFile
-    .map(line => {
-      ((line._2,line._8),1)
-    })
-    .reduceByKey(_+_)
-    .saveAsTextFile("productsOfCompanies.txt")
+//     textFile
+//     .map(line => {
+//       ((line._2,line._8),1)
+//     })
+//     .reduceByKey(_+_)
+//     .saveAsTextFile("productsOfCompanies.txt")
     
-    textFile
-    .map(line => {
-      var yes = 0
-      var no = 0
-      var na = 0
-      if(line._17 == "Yes") yes = 1
-      else if(line._17 == "No") no = 1
-      else na = 1
-      (line._9, (yes, no, na))
-    })
-    .reduceByKey((v1,v2) => {
-      (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3)
-    })
-    .saveAsTextFile("StatesConsumerDisputed.txt")
+//     textFile
+//     .map(line => {
+//       var yes = 0
+//       var no = 0
+//       var na = 0
+//       if(line._17 == "Yes") yes = 1
+//       else if(line._17 == "No") no = 1
+//       else na = 1
+//       (line._9, (yes, no, na))
+//     })
+//     .reduceByKey((v1,v2) => {
+//       (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3)
+//     })
+//     .saveAsTextFile("StatesConsumerDisputed.txt")
     
-    textFile
-    .map(line => {
-      ((line._9, line._12),1)
-    })
-    .reduceByKey(_+_)
-    .saveAsTextFile("HowSubmitted.txt")
+//     textFile
+//     .map(line => {
+//       ((line._9, line._12),1)
+//     })
+//     .reduceByKey(_+_)
+//     .saveAsTextFile("HowSubmitted.txt")
     
-    textFile
-    .map(line => {
-      var yes = 0
-      var no = 0
-      var na = 0
-      if(line._17 == "Yes") yes = 1
-      else if(line._17 == "No") no = 1
-      else na = 1
-      (line._2, (yes, no, na))
-    })
-    .reduceByKey((v1,v2) => {
-      (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3)
-    })
-    .saveAsTextFile("ProductDispute.txt")
+//     textFile
+//     .map(line => {
+//       var yes = 0
+//       var no = 0
+//       var na = 0
+//       if(line._17 == "Yes") yes = 1
+//       else if(line._17 == "No") no = 1
+//       else na = 1
+//       (line._2, (yes, no, na))
+//     })
+//     .reduceByKey((v1,v2) => {
+//       (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3)
+//     })
+//     .saveAsTextFile("ProductDispute.txt")
     
-    textFile
-    .map(line => {
-      var yes = 0
-      var no = 0
-      if(line._16 == "Yes") yes = 1
-      else no = 1
-      (line._9, (yes, no))
-    })
-    .reduceByKey((v1,v2) => {
-      (v1._1 + v2._1, v1._2 + v2._2)
-    })
-    .saveAsTextFile("StateTimelyResponse.txt")
+//     textFile
+//     .map(line => {
+//       var yes = 0
+//       var no = 0
+//       if(line._16 == "Yes") yes = 1
+//       else no = 1
+//       (line._9, (yes, no))
+//     })
+//     .reduceByKey((v1,v2) => {
+//       (v1._1 + v2._1, v1._2 + v2._2)
+//     })
+//     .saveAsTextFile("StateTimelyResponse.txt")
   }
 }
 

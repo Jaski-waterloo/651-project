29664e4ea3096eaa2e37130e81858aa5b11f262f
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-11-25 17:21:47 -0500

    Update test.scala

--- src/main/scala/ca/uwaterloo/cs451/project/test.scala
+++ src/main/scala/ca/uwaterloo/cs451/project/test.scala
@@ -46,73 +46,72 @@ object test extends Tokenizer {
     .take(5)
     .map(line => {
       val tokens = line.split(',')
-      (tokens(0),tokens(1))
+      (tokens(1),1)
     })
-    .foreach(line => {
-      println(line)
+    .reduceByKey(_+_)
+    .saveAsTextFile("numberOfProducts.txt")
+    
+    textFile
+    .map(line => {
+      val tokens = line.split(',')
+      ((tokens(7),tokens(1)),1)
     })
-//     .map(line => {
-//       (line(1), 1)
-//     })
-//     .reduceByKey(_+_)
-//     .saveAsTextFile("numberOfProducts.txt")
+    .reduceByKey(_+_)
+    .saveAsTextFile("productsOfCompanies.txt")
     
-//     textFile
-//     .map(line => {
-//       ((line._2,line._8),1)
-//     })
-//     .reduceByKey(_+_)
-//     .saveAsTextFile("productsOfCompanies.txt")
+    textFile
+    .map(line => {
+      val tokens = line.split(',')
+      var yes = 0
+      var no = 0
+      var na = 0
+      if(tokens(16) == "Yes") yes = 1
+      else if(tokens(16) == "No") no = 1
+      else na = 1
+      (tokens(8), (yes, no, na))
+    })
+    .reduceByKey((v1,v2) => {
+      (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3)
+    })
+    .saveAsTextFile("StatesConsumerDisputed.txt")
     
-//     textFile
-//     .map(line => {
-//       var yes = 0
-//       var no = 0
-//       var na = 0
-//       if(line._17 == "Yes") yes = 1
-//       else if(line._17 == "No") no = 1
-//       else na = 1
-//       (line._9, (yes, no, na))
-//     })
-//     .reduceByKey((v1,v2) => {
-//       (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3)
-//     })
-//     .saveAsTextFile("StatesConsumerDisputed.txt")
+    textFile
+    .map(line => {
+      val tokens = line.split(',')
+      ((tokens(8), tokens(11),1)
+    })
+    .reduceByKey(_+_)
+    .saveAsTextFile("HowSubmitted.txt")
     
-//     textFile
-//     .map(line => {
-//       ((line._9, line._12),1)
-//     })
-//     .reduceByKey(_+_)
-//     .saveAsTextFile("HowSubmitted.txt")
+    textFile
+    .map(line => {
+      val tokens = line.split(',')
+      var yes = 0
+      var no = 0
+      var na = 0
+      if(tokens(16) == "Yes") yes = 1
+      else if(tokens(16) == "No") no = 1
+      else na = 1
+      (tokens(8), (yes, no, na))
+    })
+    .reduceByKey((v1,v2) => {
+      (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3)
+    })
+    .saveAsTextFile("ProductDispute.txt")
     
-//     textFile
-//     .map(line => {
-//       var yes = 0
-//       var no = 0
-//       var na = 0
-//       if(line._17 == "Yes") yes = 1
-//       else if(line._17 == "No") no = 1
-//       else na = 1
-//       (line._2, (yes, no, na))
-//     })
-//     .reduceByKey((v1,v2) => {
-//       (v1._1 + v2._1, v1._2 + v2._2, v1._3 + v2._3)
-//     })
-//     .saveAsTextFile("ProductDispute.txt")
-    
-//     textFile
-//     .map(line => {
-//       var yes = 0
-//       var no = 0
-//       if(line._16 == "Yes") yes = 1
-//       else no = 1
-//       (line._9, (yes, no))
-//     })
-//     .reduceByKey((v1,v2) => {
-//       (v1._1 + v2._1, v1._2 + v2._2)
-//     })
-//     .saveAsTextFile("StateTimelyResponse.txt")
+    textFile
+    .map(line => {
+      val tokens = line.split(',')
+      var yes = 0
+      var no = 0
+      if(tokens(15) == "Yes") yes = 1
+      else no = 1
+      (tokens(8), (yes, no))
+    })
+    .reduceByKey((v1,v2) => {
+      (v1._1 + v2._1, v1._2 + v2._2)
+    })
+    .saveAsTextFile("StateTimelyResponse.txt")
   }
 }
 
